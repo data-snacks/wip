101324681b3ce8b85932fdedc2459949c744801c
DataEngineeringTech <diegoeula.endava@gmail.com>
2024-05-21 23:08:05 -0300

    add changes to airflow orchestration

--- coinbase-project/dags/coinbase.py
+++ coinbase-project/dags/coinbase.py
@@ -1,10 +1,11 @@
 from datetime import timedelta, datetime
 from airflow import DAG
+from airflow.operators.dummy_operator import DummyOperator
 from airflow.operators.python_operator import PythonOperator
 from include.coinbase_fetcher import produce_data
 from include.kafka_to_minio import consume_data
 
-start_date = datetime(2018, 12, 21, 12, 12)
+start_date = datetime(2024, 5, 21, 12, 00)
 
 default_args = {
     'owner': 'airflow',
@@ -15,8 +16,13 @@ default_args = {
 
 with DAG('crypto_data_stream', 
          default_args=default_args, 
-         schedule_interval='*/10 * * * *', 
+         schedule_interval=None, 
          catchup=False) as dag:
+
+    start_task = DummyOperator(
+        task_id='start',
+        dag=dag,
+    )
 
     produce_data_task = PythonOperator(
         task_id='produce_data',
@@ -30,4 +36,9 @@ with DAG('crypto_data_stream',
         dag=dag,
     )
 
-    produce_data >> consume_data
+    end_task = DummyOperator(
+        task_id='end',
+        dag=dag,
+    )
+
+    start_task >> produce_data_task >> consume_data_task >> end_task
--- coinbase-project/dags/include/coinbase_fetcher.py
+++ coinbase-project/dags/include/coinbase_fetcher.py
@@ -3,7 +3,7 @@ import time
 from kafka import KafkaProducer
 from datetime import datetime
 import json
-import logging
+#import logging
 import os
 
 # COINBASE
@@ -13,33 +13,30 @@ COINBASE_API_URL=os.getenv('COINBASE_API_URL','https://api.coinbase.com/v2/price
 KAFKA_TOPIC=os.getenv('KAFKA_TOPIC','coinbase-topic')
 KAFKA_BOOTSTRAP_SERVERS=os.getenv('KAFKA_BOOTSTRAP_SERVERS','kafka:9092')
 
-producer = KafkaProducer(bootstrap_servers=KAFKA_BOOTSTRAP_SERVERS,
-                         api_version=(0,11,5),
-                         value_serializer=lambda x: json.dumps(x).encode('utf-8'))
+
 
 def fetch_coinbase_data():
     response = requests.get(COINBASE_API_URL)
     if response.status_code == 200:
+        print(f"Success to fetch data: {response.text}") 
         response = response.json()
         return response
     else:
-        logger.error(f"Failed to fetch data: {response.text}")
+        print(f"Failed to fetch data: {response.text}") 
+        #logger.error(f"Failed to fetch data: {response.text}")
         return None
 
-
 def produce_data():
-    while True:
-        data = fetch_coinbase_data()
-        if data:
-            producer.send(KAFKA_TOPIC, data)
-            now = datetime.now()
-            dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
-            logger.info(f"Data sent to Kafka: {data} at : {dt_string}")
-        time.sleep(10)  # Fetch data every 10 seconds
-
+    producer = KafkaProducer(bootstrap_servers=KAFKA_BOOTSTRAP_SERVERS,
+                        value_serializer=lambda v: json.dumps(v).encode('utf-8'))
+    data = fetch_coinbase_data()
+    if data:
+        producer.send(KAFKA_TOPIC, data)
+        now = datetime.now()
+        dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
+        print(dt_string)
+        #logger.info(f"Data sent to Kafka: {data} at : {dt_string}")
+        #time.sleep(10)  # Fetch data every 10 seconds
 
 if __name__ == "__main__":
-    logger = logging.getLogger(__name__)
-    logging.basicConfig(filename='example.log', encoding='utf-8', level=logging.INFO)
-    logger.info("This is the main")
     produce_data()
